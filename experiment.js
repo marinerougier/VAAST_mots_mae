dc3f9d842cebfbc3fe521733035cdffc904c3613
Cedric Batailler <cedric.batailler@hotmail.com>
2018-06-20 17:14:40 +0200

    Instuctions - Reference to test and thaining phases
    Co-Authored-By: braudm <braudm@users.noreply.github.com>

--- experiment.js
+++ experiment.js
@@ -451,6 +451,10 @@ var vaast_instructions_4 = {
     "<p class='instructions'>Il est très important de vous souvenir de ces consignes pour pouvoir " +
     "réaliser la tâche correctement. Il est également EXTRÊMEMENT important d'essayer de répondre " +
     "<strong>le plus rapidement et le plus exactement possible</strong>." +
+
+    "<p class ='instructions'>Vous allez commencer par une phase d'entraînement.</p>" +
+    "<p class ='instructions'><u>ATTENTION</u> : nous vous signalerons vos erreurs uniquement " +
+    "dans la phase d'entraînement, donc relisez et mémorisez bien les consignes ci-dessus." +
      "<table align='center' class='vaast-table-instruction'>" +
        "<tr>" +
          "<th>Approach</th>" +
@@ -466,12 +470,19 @@ var vaast_instructions_4 = {
 var vaast_instructions_5 = {
   type: "html-keyboard-response",
   stimulus:
-    "<h1 class ='custom-title'> Task 1 </h1>" +
-    "<p class='instructions'>The training block is now over. </p>" +
-    "<p class='instructions'>You will now have to repeat this task for 10 new blocks." +
-    " The instructions are the same as in the block you just completed." +
-    "</p>" +
-    "<p class = 'continue-instructions'>Press <strong>space</strong> to start block n°1.</p>",
+    "<h1 class ='custom-title'> Tâche </h1>" +
+    "<p class='instructions'>L'entraînement est maintenant terminé. </p>" +
+    "<p class='instructions'><u>ATTENTION</u> : vous n'aurez plus de messages pour signaler vos erreurs.</p>" +
+    "<p class='instructions'>Donc rappelez-vous bien, vous devez :</p>" +
+    "<ul class='instructions'>" +
+     "<li>" +
+      "<strong>ALLER VERS les mots " + gender_to_approach + " (en appuyant sur Y)</strong>" +
+     "</li>" +
+     "<li>" +
+      "<strong>VOUS ÉLOIGNER des mots " + gender_to_avoid + " (en appuyant sur N)</strong>" +
+     "</li>" +
+    "</ul>" +
+    "<p class = 'continue-instructions'>Appuyez sur <strong>espace</strong> pour commencer la tâche.</p>",
   choices: [32]
 };
 
